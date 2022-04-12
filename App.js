f00a09a7f51c8fbdeb14b75263a91e5c2a22b591
Bhakti Daraniya <180540107035@darshan.ac.in>
2022-04-12 10:12:50 +0530

    flex-row-design

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textYou}>You</Text>
+      <Image style={styles.iconDp} source={require('./assets/R.jpeg')}/>
+      <Image style={styles.iconMic} source={require('./assets/baseline_mic_off_white_24dp.png')}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +15,28 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop:30,
+    padding:10,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection:'row',
+    backgroundColor: '#000',
+    justifyContent:'space-between'
   },
+  textYou:{
+      color:'white',
+      alignSelf:'flex-end',
+      fontSize:18,
+     
+  },
+  iconDp:{
+    alignSelf:'center',
+    width:150,
+    height:150,
+  },
+  iconMic:{
+    marginTop:20,
+    width:30,
+    height:30,
+    
+  }
 });
