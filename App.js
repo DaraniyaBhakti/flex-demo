54de0a4868c5d9633809cd5e872bac2d02cedad4
Bhakti Daraniya <180540107035@darshan.ac.in>
2022-04-12 10:27:16 +0530

    flex-design-column

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     
+      <Image style={styles.iconMic} source={require('./assets/baseline_mic_off_white_24dp.png')}/>
+      <Image style={styles.iconDp} source={require('./assets/R.jpeg')}/>
+      <Text style={styles.textYou}>You</Text> 
+      <StatusBar style="light" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+ container: {
+    paddingTop:30,
+    padding:10,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection:'column',
+    backgroundColor: '#000',
+    justifyContent:'space-between'
   },
+  textYou:{
+      color:'white',
+      alignSelf:'flex-start',
+      fontSize:18,
+     
+  },
+  iconDp:{
+    alignSelf:'center',
+    width:150,
+    height:150,
+  },
+  iconMic:{
+    alignSelf:'flex-end',
+    marginTop:20,
+    width:30,
+    height:30,
+    
+  }
 });
+
